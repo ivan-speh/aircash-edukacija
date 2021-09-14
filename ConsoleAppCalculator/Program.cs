82b86c3980523e4d212dbb54962027acb95865cc
Ivan Špeh <ivan.speh@gmail.com>
2021-09-14 16:04:52 +0200

    Add project files.

---
+++ ConsoleAppCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
